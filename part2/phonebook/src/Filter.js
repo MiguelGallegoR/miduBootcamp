feef874c36ae378fc2886c37ba7afd0236a465dd
Miguel Gallego <gallegorodriguezmiguel58@gmail.com>
2022-11-18 11:25:12 +0100

    finished

--- part2/phonebook/src/Filter.js
+++ part2/phonebook/src/Filter.js
@@ -1,14 +1,16 @@
 import { Contact } from "./Contact"
 export const Filter = ({persons, result, changeResult}) =>{
     let search =  persons.filter((person)=>{
-        return person.name.includes(result) 
+        if(result !== ''){
+            return person.name.includes(result) 
+        }
+        
    })
    
-   console.log(persons)
    
     return (
         <div>
-            <div>escribe algo que <input onChange={changeResult} value={result} /> </div>
+            <div>filter shown with <input onChange={changeResult} value={result} /> </div>
 
             <div>
             {search.map((s)=>{
